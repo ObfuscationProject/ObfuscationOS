fc405e38a42eb974592abd41458b028b17c86599
Tab Nahida <tabye320@outlook.com>
2025-12-25 03:36:36 +0000

    Revert "first run"

--- src/kernel/entry.h
+++
@@ -1,9 +0,0 @@
-#ifndef KERNEL_ENTRY_H
-#define KERNEL_ENTRY_H
-
-#include "multiboot2.h"
-
-// 内核入口点声明
-extern "C" void kernel_main(unsigned long multiboot_info, unsigned long magic);
-
-#endif // KERNEL_ENTRY_H
